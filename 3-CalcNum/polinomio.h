5c825c28d1e6327b94b30b43d02e661c76d4abec
newtmagalhaes <aniltonmagalhaes@gmail.com>
2021-06-13 13:40:51 -0300

    Adicionando teorema de lagrange e método de briot-ruffini

--- 3-CalcNum/polinomio.h
+++ 3-CalcNum/polinomio.h
@@ -3,6 +3,7 @@
 #pragma once
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 struct Polinomio
 {
@@ -29,32 +30,134 @@ Polinomio *alocar_polinomio(unsigned n_termos)
 }
 
 /**
- * Aloca e preenche o polinômio conforme inputs da linha de comando
+ * Preenche o polinômio conforme inputs da linha de comando
  * @param p polinômio a ser preenchido.
 */
-Polinomio *alocar_polinomio_preenchido()
+void ler_polinomio(Polinomio *p)
 {
-  unsigned n;
-  printf("Digite a quantidade de termos do polinomio: ");
-  scanf("%u", &n);
-
-  Polinomio *p = alocar_polinomio(n);
-  if (p == NULL)
-  {
-    return NULL;
-  }
-
   for (int i = p->n_coefs - 1; i >= 0; i--)
   {
     printf("a%u = ", i);
     scanf("%lf", &p->coefs[i]);
   }
+}
 
-  return p;
+// Executa o Método de Briot-Ruffini
+double metodo_briot_ruffini(Polinomio *p, double x, unsigned i)
+{
+  if (i == p->n_coefs - 1)
+  {
+    return p->coefs[i];
+  }
+  else
+  {
+    return x * metodo_briot_ruffini(p, x, i + 1) + p->coefs[i];
+  }
 }
 
 // Computa P(x)
-double eval(Polinomio *p, double x);
+double eval(Polinomio *p, double x)
+{
+  return metodo_briot_ruffini(p, x, 0);
+}
+
+// usado em _L_teorema_de_lagrange
+typedef enum lagrange
+{
+  L0, // não modificar p
+  L1, // modificar p para obter p1(x) = x^n * p(1/x);
+  L2, // modificar p para obter p2(x) = p(-x)
+  L3  // modificar p para obter p3(x) = x^n * p(-1/x);
+} _L_Lagrange;
+
+// modificar p para obter x^n * p(1/x);
+void _inverter_ordem_de_coeficientes(Polinomio *p)
+{
+  int n = p->n_coefs - 1;
+  double aux;
+  for (int i = 0; i < n / 2; i++)
+  {
+    aux = p->coefs[i];
+    p->coefs[i] = p->coefs[n - i];
+    p->coefs[n - i] = aux;
+  }
+}
+
+// modificar p para obter p(-x)
+void _simetrico_indices_impares(Polinomio *p)
+{
+  for (int i = 1; i < p->n_coefs; i += 2)
+  {
+    p->coefs[i] = -p->coefs[i];
+  }
+}
+
+double _L_teorema_de_lagrange(Polinomio *p, _L_Lagrange l)
+{
+  // Fazer alterações necessárias conforme o retorno desejado
+  if (l == L3 || l == L2)
+  {
+    // (alterar o sinal dos coeficientes de índice ímpar)
+    _simetrico_indices_impares(p);
+  }
+  if (l == L3 || l == L1)
+  {
+    // (inverter a ordem dos coeficientes)
+    _inverter_ordem_de_coeficientes(p);
+  }
+
+  int n = p->n_coefs - 1,
+      desfazer_oposicao = 0;
+  // SE an < 0, p(x) é redefinido para -p(x)
+  if (p->coefs[n] < 0)
+  {
+    desfazer_oposicao = 1;
+    for (int i = 0; i < p->n_coefs; i++)
+    {
+      p->coefs[i] = -p->coefs[i];
+    }
+  }
+
+  // Definir k e B
+  int k = 0;    // o maior índices dos coeficientes negativos;
+  double B = 0, // o módulo do menor coeficiente negativo;
+      an = p->coefs[n];
+  for (int i = p->n_coefs - 1; i >= 0; i--)
+  {
+    if (p->coefs[i] < 0)
+    {
+      if (i > k)
+      {
+        k = i;
+      }
+      if (abs(p->coefs[i]) > B)
+      {
+        B = abs(p->coefs[i]);
+      }
+    }
+  }
+
+  // Desfazendo alterações recuperando o polinômio original
+  if (desfazer_oposicao)
+  {
+    for (int i = 0; i < p->n_coefs; i++)
+    {
+      p->coefs[i] = -p->coefs[i];
+    }
+  }
+  if (l == L3 || l == L1)
+  {
+    // (inverter a ordem dos coeficientes)
+    _inverter_ordem_de_coeficientes(p);
+  }
+  if (l == L3 || l == L2)
+  {
+    // (alterar o sinal dos coeficientes de índice ímpar)
+    _simetrico_indices_impares(p);
+  }
+
+  return 1 + pow(B / an, 1.0 / (n - k));
+}
 
 void mostrar_polinomio(Polinomio *p)
 {
